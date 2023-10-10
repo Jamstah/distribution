f34d1822d07b26f5b98abd9a00404e1a52658243
Milos Gajdos <milosthegajdos@gmail.com>
2023-10-10 19:20:47 +0100

    Move code from certificats.go into token_test.go
    This code is only used in token_test.go file so there is
    no need for it to be in a dedicated file.
    
    Signed-off-by: Milos Gajdos <milosthegajdos@gmail.com>

--- registry/auth/token/token_test.go
+++ registry/auth/token/token_test.go
@@ -6,9 +6,12 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
+	"math/big"
+	"net"
 	"net/http"
 	"os"
 	"testing"
@@ -49,7 +52,7 @@ func makeRootCerts(rootKeys []key) ([]*x509.Certificate, error) {
 	rootCerts := make([]*x509.Certificate, 0, len(rootKeys))
 
 	for _, rootKey := range rootKeys {
-		cert, err := GenerateCACert(rootKey, rootKey)
+		cert, err := generateCACert(rootKey, rootKey)
 		if err != nil {
 			return nil, err
 		}
@@ -84,7 +87,7 @@ func makeSigningKeyWithChain(rootKey key, depth int) (*jose.JSONWebKey, error) {
 		}
 
 		trustedKey := key{priv: pk, pub: pk.Public()}
-		if cert, err = GenerateCACert(parentKey, trustedKey); err != nil {
+		if cert, err = generateCACert(parentKey, trustedKey); err != nil {
 			return nil, err
 		}
 
@@ -143,6 +146,91 @@ func makeTestToken(issuer, audience string, access []*ResourceActions, rootKey k
 	}
 
 	return NewToken(tokenString)
+}
+
+// NOTE(milosgajdos): certTemplateInfo type as well
+// as some of the functions in this file have been
+// adopted from https://github.com/docker/libtrust
+// and modiified to fit the purpose of the token package.
+
+type certTemplateInfo struct {
+	commonName  string
+	domains     []string
+	ipAddresses []net.IP
+	isCA        bool
+	clientAuth  bool
+	serverAuth  bool
+}
+
+func generateCertTemplate(info *certTemplateInfo) *x509.Certificate {
+	// Generate a certificate template which is valid from the past week to
+	// 10 years from now. The usage of the certificate depends on the
+	// specified fields in the given certTempInfo object.
+	var (
+		keyUsage    x509.KeyUsage
+		extKeyUsage []x509.ExtKeyUsage
+	)
+
+	if info.isCA {
+		keyUsage = x509.KeyUsageCertSign
+	}
+
+	if info.clientAuth {
+		extKeyUsage = append(extKeyUsage, x509.ExtKeyUsageClientAuth)
+	}
+
+	if info.serverAuth {
+		extKeyUsage = append(extKeyUsage, x509.ExtKeyUsageServerAuth)
+	}
+
+	return &x509.Certificate{
+		SerialNumber: big.NewInt(0),
+		Subject: pkix.Name{
+			CommonName: info.commonName,
+		},
+		NotBefore:             time.Now().Add(-time.Hour * 24 * 7),
+		NotAfter:              time.Now().Add(time.Hour * 24 * 365 * 10),
+		DNSNames:              info.domains,
+		IPAddresses:           info.ipAddresses,
+		IsCA:                  info.isCA,
+		KeyUsage:              keyUsage,
+		ExtKeyUsage:           extKeyUsage,
+		BasicConstraintsValid: info.isCA,
+	}
+}
+
+func generateCert(priv crypto.PrivateKey, pub crypto.PublicKey, subInfo, issInfo *certTemplateInfo) (*x509.Certificate, error) {
+	pubCertTemplate := generateCertTemplate(subInfo)
+	privCertTemplate := generateCertTemplate(issInfo)
+
+	certDER, err := x509.CreateCertificate(
+		rand.Reader, pubCertTemplate, privCertTemplate,
+		pub, priv,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create certificate: %s", err)
+	}
+
+	cert, err := x509.ParseCertificate(certDER)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse certificate: %s", err)
+	}
+
+	return cert, nil
+}
+
+// generateCACert creates a certificate which can be used as a trusted
+// certificate authority.
+func generateCACert(signer key, trustedKey key) (*x509.Certificate, error) {
+	subjectInfo := &certTemplateInfo{
+		commonName: trustedKey.KeyID(),
+		isCA:       true,
+	}
+	issuerInfo := &certTemplateInfo{
+		commonName: signer.KeyID(),
+	}
+
+	return generateCert(signer.priv, trustedKey.pub, subjectInfo, issuerInfo)
 }
 
 // This test makes 4 tokens with a varying number of intermediate
